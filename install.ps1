408057ab382e3bf8fa506adc0ff9fd15343df4e3
roiseuxquentin <quentin@roiseux.fr>
2020-04-13 12:24:18 +0200

    ok
    ok

--- install.ps1
+++ install.ps1
@@ -8,7 +8,6 @@ Add-Type -AssemblyName System.Windows.Forms
 
 # recup pdf tools
  wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox-0.12.5-1.msvc2015-win64.exe -OutFile install.exe
- wget https://github.com/Roiseuxquentin/EstivalWorkTool/blob/master/generatePDF.exe  -OutFile generatePDF.exe
 
 # INSTALL PDF TOOLS
  Start-Process '.\install.exe' "/S" -Verb runAs
