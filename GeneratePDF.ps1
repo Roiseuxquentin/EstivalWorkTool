29eeb3797b87a6e5feaa482f34b041506bf004c8
roiseuxquentin <quentin@roiseux.fr>
2020-04-13 11:05:37 +0200

    add files
    add files

---
+++ GeneratePDF.ps1
@@ -0,0 +1,35 @@
+﻿# Script Html to Pdf
+# Pour Estival 
+# eegloo 2020
+# quentin@roiseux.fr
+
+# ADD MODULE
+Add-Type -AssemblyName System.Windows.Forms
+
+# PDF tool Path into variable
+$PDFtoolPath = 'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+
+# INPUT HTML FOLDER
+[void][System.Windows.Forms.MessageBox]::Show("ETAPE 1 : Selection du dossier project_sheets contenant les fichiers HTML.", "PDF" , 0, 32)
+$FolderInput = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{ ShowNewFolderButton = $false }
+[void]$FolderInput.ShowDialog()
+
+# OUTPUT PDF FOLDER
+[void][System.Windows.Forms.MessageBox]::Show("ETAPE 2 : Selection du dossier pour la création du fichier PDF.", "PDF" , 0, 32)
+$FolderOutput = New-Object System.Windows.Forms.FolderBrowserDialog
+[void]$FolderOutput.ShowDialog()
+
+$FileList = @()
+
+# READ HTML FILE LIST FROM SELECTED FOLDER
+ls $FolderInput.SelectedPath  | Where-Object {$_.Extension -eq ".html"} |  Foreach-Object {
+          $file = "$($FolderInput.SelectedPath)\$($_.Name)"
+          $FileList += $file                        
+}
+
+# MERGE MULTIPLE PDF TO SINGLE PDF
+# WRITE PDF FILE TO SELECTED FOLDER
+& $PDFtoolPath $fileList "$($FolderOutput.SelectedPath)\output.pdf"
+
+# END MSG
+[void][System.Windows.Forms.MessageBox]::Show("Le fichier PDF a été généré dans $($FolderOutput.SelectedPath)\ ", "PDF" , 0, 64)
---
+++ install.ps1
@@ -0,0 +1,24 @@
+﻿# Script Html to Pdf
+# Pour Estival 
+# eegloo 2020
+# quentin@roiseux.fr
+
+# ADD MODULE
+Add-Type -AssemblyName System.Windows.Forms
+
+# recup pdf tools
+ wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox-0.12.5-1.msvc2015-win64.exe -OutFile install.exe
+
+# INSTALL PDF TOOLS
+ Start-Process '.\install.exe' "/S" -Verb runAs
+
+ sleep 5
+
+# DELETE install
+ Remove-Item '.\install.exe'
+
+# DOWNLOAD SCRIPT
+ #wget mongithub/script.exe -OutFile install.exe
+
+# DIALOG POUR LA DESTINATION DU FICHIER
+[void][System.Windows.Forms.MessageBox]::Show("L'installation est terminée, lancer le fichier 'GeneratePDF.exe' pour migrer les fichiers HTML du dossier 'projet_sheets/' en PDF.", "PDF" , 0, 64)
